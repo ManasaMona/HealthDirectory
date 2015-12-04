1ba05a68fbdd5bd5e0f38dffc219f27d0a7ec7ac
ManasaMona <manasaalisetty@gamil.com>
2015-12-04 09:13:39 +0530

    final changes

--- myapps/healthDirectory/views.py
+++ myapps/healthDirectory/views.py
@@ -10,7 +10,14 @@ from django.template import RequestContext, loader
 from django.db.models import Q
 
 def index(request):
-        return render(request, 'healthDirectory/homepage.html')
+		ser = Service_provider.objects.all().order_by('-id')[:7]
+		template = loader.get_template('healthDirectory/homepage.html')
+		context = RequestContext(request, {
+		        'ser': ser,
+		})
+		return HttpResponse(template.render(context))
+		
+        #return render(request, 'healthDirectory/homepage.html')
         #return render(request, 'healthDirectory/regis-login.html')
 	#return render(request, 'healthDirectory/regis-login.html')
 
@@ -30,9 +37,9 @@ def validate(request):
 	if not Service_provider.objects.filter(ser_email=ser_email):
 		s = Service_provider(ser_name=ser_name,ser_phNum=ser_phNum,ser_spl=ser_spl,ser_psword=ser_psword,ser_location=ser_location,ser_addr=ser_addr,ser_email=ser_email)
 		s.save()
-		response['status'] = 'sucess'
+		response['status'] = 'Registration is Sucessfull'
 	else:
-		response['status'] = 'failure'
+		response['status'] = 'Registration Failed! Email already exists'
 	json_data = json.dumps(response)
 	return HttpResponse(json_data, content_type = "application/json")
 
@@ -48,7 +55,7 @@ def validatelogin(request):
 		})
 		return HttpResponse(template.render(context))
 	else:
-		return render_to_response('healthDirectory/regis-login.html')
+		return render_to_response('healthDirectory/regis-login.html', {'title':' Email and Password does not match!'},context_instance=RequestContext(request))
 
 def searchresults(request) :
 		ser_value=request.POST.get('searchvalue')
@@ -90,18 +97,18 @@ def updateprofile(request):
                                 return HttpResponse(response)
 		
                 if ser_email:
-			if ser_name:
-				Service_provider.objects.filter(ser_email=ser_email).update(ser_name=ser_name) 
-			if ser_spl:
-				Service_provider.objects.filter(ser_email=ser_email).update(ser_spl=ser_spl)
-			if ser_location:
-				Service_provider.objects.filter(ser_email=ser_email).update(ser_location=ser_location)
-			if ser_phNum:
-				Service_provider.objects.filter(ser_email=ser_email).update(ser_phNum=ser_phNum)
-			if ser_psword:
-				Service_provider.objects.filter(ser_email=ser_email).update(ser_psword=ser_psword)
-			if ser_addr:
-				Service_provider.objects.filter(ser_email=ser_email).update(ser_addr=ser_addr)
+					if ser_name:
+						Service_provider.objects.filter(ser_email=ser_email).update(ser_name=ser_name) 
+					if ser_spl:
+						Service_provider.objects.filter(ser_email=ser_email).update(ser_spl=ser_spl)
+					if ser_location:
+						Service_provider.objects.filter(ser_email=ser_email).update(ser_location=ser_location)
+					if ser_phNum:
+						Service_provider.objects.filter(ser_email=ser_email).update(ser_phNum=ser_phNum)
+					if ser_psword:
+						Service_provider.objects.filter(ser_email=ser_email).update(ser_psword=ser_psword)
+					if ser_addr:
+						Service_provider.objects.filter(ser_email=ser_email).update(ser_addr=ser_addr)
 						
 		
 		ser=Service_provider.objects.filter(ser_email=ser_email)
